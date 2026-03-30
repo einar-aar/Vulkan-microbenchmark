b3c5cfa5ea051d9e5a278c8d08fad45803dec160
einar-aar <einar@skogn.no>
2026-03-30 14:24:31 +0200

    Vulkan infrastructure source done

--- headers/vulkan_infrastucture.hpp
+++ headers/vulkan_infrastucture.hpp
@@ -5,7 +5,7 @@
 #include <optional>
 #include <vector>
 
-class VKInfrastructure {
+class VkInfrastructure {
 
 private:
 
@@ -15,18 +15,24 @@ private:
     VkInstance instance = VK_NULL_HANDLE;
     VkCommandPool cmdPool = VK_NULL_HANDLE;
     VkQueue queue = VK_NULL_HANDLE;
-    uint32_t queueFamilyIndex = 0;
+    uint32_t computeQueueFamilyIndex = 0;
+
+    // Struct to save temporary family index for relevant queues
+    struct QueueFamilyIndex {
+        std::optional<uint32_t> familyIndex;
+        bool hasValue() const { return familyIndex.has_value(); }
+    };
 
 public:
     
     // PUBLIC FUNCTIONS
-    VKInfrastructure();
-    ~VKInfrastructure();
+    VkInfrastructure();
+    ~VkInfrastructure();
 
     // DO NOT allow copying, as this leads to undefined behaviour when you have multiple objects
     // owning the same handles. (potential double destruction)
-    VKInfrastructure& operator= (const VKInfrastructure&) = delete;
-    VKInfrastructure(const VKInfrastructure&) = delete;
+    VkInfrastructure& operator= (const VkInfrastructure&) = delete;
+    VkInfrastructure(const VkInfrastructure&) = delete;
 
     // Getter functions
     VkPhysicalDevice getPhysicalDev() const { return physicalDev; }
@@ -34,23 +40,18 @@ public:
     VkInstance getVKInstance() const { return instance; }
     VkCommandPool getCmdPool() const { return cmdPool; }
     VkQueue getQueue() const { return queue; }
-    uint32_t getQueueFamilyIndex() const { return queueFamilyIndex; }
+    uint32_t getComputeQueueFamilyIndex() const { return computeQueueFamilyIndex; }
 
 private:
 
     // PRIVATE FUNCTIONS
     // Choosing physical and creating logical device
     void choosePhysicalDev();
-    QueueFamilyTempIndex getQueueFamilies(VkPhysicalDevice dev) const;
+    QueueFamilyIndex getQueueFamilies(VkPhysicalDevice dev) const;
     bool checkDevSuitability(VkPhysicalDevice dev) const;
     void createLogicalDev();
 
     // Setting up other infrastructure
     void createInstance();
     void createCmdPool();
-
-    struct QueueFamilyTempIndex {
-        std::optional<uint32_t> family;
-        bool hasValue() const { return family.has_value(); }
-    };
 };
